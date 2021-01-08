3442a5e3902cab60fb1be97cf678ab14fc5662b3
INVOKER <mattbrodowski1@yahoo.com>
2021-01-07 21:48:06 -0500

    keyup/keydown detection
    8 bools to keepo track of arrow keys

--- tilengine test/tilengine test/game.cpp
+++ tilengine test/tilengine test/game.cpp
@@ -11,6 +11,18 @@ void Game::init()
 	TLN_DefineInputKey(PLAYER1, INPUT_BUTTON5, SDLK_p);
 }
 
+bool Game::inputtest()
+{
+	if (TLN_GetInput(INPUT_LEFT))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 Game::Game()
 {
 	init();
@@ -39,7 +51,8 @@ void Game::gameLoop()
 	spriteIndex++;
 	Marker mMarker(spriteIndex);
 	spriteIndex++;
-	
+	bool downstate = false;
+	bool downstateold = false;
 
 	/* main loop */
 	TLN_CreateWindow(NULL, 0);
@@ -47,40 +60,66 @@ void Game::gameLoop()
 
 		while (TLN_ProcessWindow())
 		{
-			
 			//NOT PAUSED
 			if (!isPause)
 			{
 				if (!cuckie.getWalk())
 				{
-					if (TLN_GetInput(INPUT_LEFT))
+					
+					if (TLN_GetInput(INPUT_LEFT) && leftPressOld == false )
 					{
 						cuckie.setWalk(true);
 						cuckie.setDir(CHLEFT, false);
 						cuckie.move(cuckie.getDir(), cuckie.getSpeed(), false);
-
+						leftPress = true;
+						
 					}
-					else if (TLN_GetInput(INPUT_RIGHT))
+					else if (!TLN_GetInput(INPUT_LEFT) && leftPressOld == true)
+					{
+						leftPress = false;
+					}
+					leftPressOld = leftPress;
+
+
+					if (TLN_GetInput(INPUT_RIGHT) && rightPressOld == false)
 					{
 						cuckie.setWalk(true);
 						cuckie.setDir(CHRIGHT, false);
 						cuckie.move(cuckie.getDir(), cuckie.getSpeed(), false);
+						rightPress = true;
 					}
-					else if (TLN_GetInput(INPUT_UP))
+					else if (!TLN_GetInput(INPUT_RIGHT) && rightPressOld == true)
+					{
+						rightPress = false;
+					}
+					rightPressOld = rightPress;
+
+					if (TLN_GetInput(INPUT_UP) && upPressOld == false)
 					{
 						cuckie.setWalk(true);
 						cuckie.setDir(CHUP, false);
 						cuckie.move(cuckie.getDir(), cuckie.getSpeed(), false);
+						upPress = true;
 					}
-					else if (TLN_GetInput(INPUT_DOWN))
+					else if (!TLN_GetInput(INPUT_UP) && upPressOld == true)
+					{
+						upPress = false;
+					}
+					upPressOld = upPress;
+
+					if (TLN_GetInput(INPUT_DOWN) && downPressOld == false)
 					{
 						cuckie.setWalk(true);
 						cuckie.setDir(CHDOWN, false);
 						cuckie.move(cuckie.getDir(), cuckie.getSpeed(), false);
+						upPress = true;
 					}
-					else
+					else if (!TLN_GetInput(INPUT_DOWN) && downPressOld == true)
 					{
+						downPress = false;
 					}
+					downPressOld = downPress;
+
 					cuckie.checkAnimState();
 				}
 
@@ -96,8 +135,6 @@ void Game::gameLoop()
 			}//IS PAUSED
 			else if (isPause)
 			{
-				//if (!mMarker.isMoving() && mMarker.getPixY() % aux.GRIDSIZE == 0)
-				//{
 					if (TLN_GetInput(INPUT_UP))
 					{
 						mMarker.moveMarker(true);
@@ -106,15 +143,6 @@ void Game::gameLoop()
 					{
 						mMarker.moveMarker(false);
 					}
-				//}
-				/*if (mMarker.getPixY() % aux.GRIDSIZE != 0)
-				{
-					mMarker.move();
-				}
-				else {
-					mMarker.isMarkerMoving = false;
-				}*/
-
 			}
 
 			if (TLN_GetInput(INPUT_BUTTON5))
